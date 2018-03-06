35bd2ec0f2c3f478eaf3c2394642823de8591cef
GiFree <fra.matusiak@gmail.com>
2018-03-06 11:37:10 +0100

    preparing profile site

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
+const steamUser = require('steam-user');
 
 router.get('/', (req, res, next) => {
-  res.render('profile', { text: req.decoded.username, token: req.cookies.token });
+  const data = {
+    text: req.decoded.username,
+    token: req.cookies.token
+  }
+  User.findOne({ username: req.decoded.username }, (err, user) => {
+    if (err) return next(new Error('Error user can not be found'));
+    data.password = user.password;
+    res.render('profile', { data });
+  })
+
+});
+
+router.post('/', (req, res, next) => {
+  if (read.body && req.body.steamID64) {
+    User.updateOne({ username: req.decoded.username }, {
+      $set: {
+        steamID: req.body.steamID64
+      }
+    });
+    res.json({ success: 'Steam id set successfully' });
+  }
 });
 
 module.exports = router;
