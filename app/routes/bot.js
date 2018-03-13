8bb1e19a17d376a75bc506c4cb9cbc7424ed1c0f
GiFree <fra.matusiak@gmail.com>
2018-03-13 09:34:06 +0100

    adding bot.js which controlls bot

---
+++ app/routes/bot.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const user = require('../models/user');
+const steamUser = require('steam-user');
+
+router.get('/', (req, res, next) => {
+  res.redirect(`/${req.decoded.id}`);
+})
+
+router.post('/:id/:appid', (req, res, next) => {
+
+  user.getParams(req.decoded.id, params, (err, data) => {
+    if (err) return next(err);
+    if (!data) {
+      const error = new Error('Database didnt respond');
+      error.status = 401;
+      return next(error);
+    }
+  });
+
+})
+
+
+module.exports = router;
